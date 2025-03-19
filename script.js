dbd681b379d7382c1f59b4c0f152c31c951c1446
osesunday1 <ose.sunday@gmail.com>
2025-03-19 15:40:09 -0400

    togg

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+//--------------- NavBar----------------------
+
+function toggleMenu() {
+  var dropdown = document.getElementById("mobileDropdown");
+
+  if (dropdown.classList.contains("active")) {
+      dropdown.classList.remove("active"); // Hide menu
+      setTimeout(() => {
+          dropdown.style.display = "none"; // Delay hiding for smooth effect
+      }, 400);
+  } else {
+      dropdown.style.display = "block"; // Show menu
+      setTimeout(() => {
+          dropdown.classList.add("active");
+      }, 10); // Small delay to trigger transition
+  }
+}
+
 //--------------- DOOR TABS----------------------
 
 const tabs = document.querySelector(".doorWrapper");
